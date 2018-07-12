49c30d53c00031f1db150c17e49db9f16065725c
sheelabhadra <sheelabhadra@gmail.com>
2018-07-11 23:10:38 -0500

    feat: Single audio test running YOOO

--- Paper-2/test_audio_file.py
+++ Paper-2/test_audio_file.py
@@ -1,15 +1,19 @@
 # Test whether an Emergency signal is present in an audio sample
-
 from em_detection import *
 
-test_file = 'data/eval/emergency/17.wav'
+# Read the test file
+test_file = '../Data/eval/emergency/17.wav'
 y, sr = librosa.load(test_file, sr=8000)
+
+# Load the scaler obtained from the train data
+scaler_filename = "scaler.save"
+scaler = joblib.load(scaler_filename)
 
 classes = predict_probability(y, scaler)
 
 fig = plt.figure(figsize=(8,4))
 ax = fig.add_subplot(111)
-ax.plot(classes, c='r', linewidth = 3.0, alpha=0.5)
+ax.plot(classes, c='b', linewidth = 3.0, alpha=0.5)
 scale_x = len(classes)/10.0
 ticks_x = ticker.FuncFormatter(lambda x, pos: '%.2f'%(x/scale_x))
 ax.xaxis.set_major_formatter(ticks_x)
@@ -19,4 +23,6 @@ ax.set_xlabel("time (s)")
 ax.set_ylabel("Em signal presence")
 plt.grid('on')
 plt.tight_layout()
+plt.savefig('Em_probability.png')
 plt.show()
+
